255cb427556cf94afe5eab4c7f1e330ccdfbc3a7
Apple_pine <appchen0517@gmail.com>
2025-07-19 02:44:11 +0800

    feat : Groups.vue

--- frontend/src/components/home/Groups.vue
+++ frontend/src/components/home/Groups.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="relative w-full min-h-screen overflow-hidden bg-[#2D3E63]">
     <div class="min-h-[calc(100vh-56px)] bg-[#2D3E63] flex flex-col w-full">
+      <img src="../../assets/Home/star-06-1.svg" alt="star06-1" class="absolute left-[80%]" />
+      <img
+        src="../../assets/Home/star-07-2.svg"
+        alt="star07-2"
+        class="absolute left-[75%] top-[15%]"
+      />
+      <img
+        src="../../assets/Home/star-07-1.svg"
+        alt="start07-1"
+        class="z-1 scale-[0.7] absolute bottom-[-8%] left-[-1%]"
+      />
       <!-- <div class="text-3xl font-bold text-center text-[#F4F5F5]">Workshop</div> -->
 
       <div
@@ -9,7 +20,7 @@
         組別介紹、工作坊
       </div>
 
-      <!-- 黑客組 -->
+      <!-- Card1 : 黑客組 -->
       <div class="w-[80vw] relative mx-auto select-none">
         <!-- tab ! -->
         <!-- tab 黑客組 -->
@@ -50,11 +61,11 @@
               <div class="content w-[29vw] h-[35vh] bg-zinc-300 rounded-[30px]"></div>
 
               <div
-                class="content w-[30vw] text-[#656565] text-[1.65vw] flex items-center justify-center font-bold font-[Chiron_Hei_HK]"
+                class="content w-[30vw] text-[#656565] text-[1.6vw] flex items-center justify-center font-bold font-[Chiron_Hei_HK]"
               >
                 <!-- 字置中 ! -->
                 致力於搭起學界與業界之間的橋樑，參賽者將運用企業資源並發揮創意回應企業命題，讓想像力與實作並存！2025
-                年梅竹黑客松將由以下六家企業作為引導單位：CloudMosa、恩智浦半導體、台積電、羅技、Google、AMD。
+                年梅竹黑客松將由以下六家企業作為引導單位：CloudMosa、恩智浦半導體、台積電、<br />羅技、Google、AMD。
               </div>
             </template>
 
@@ -67,10 +78,10 @@
 
               <!-- info -->
               <div
-                class="container w-[30vw] h-[35vh] flex flex-col items-center justify-center font-bold font-[Chiron_Hei_HK]"
+                class="w-[30vw] h-[35vh] flex flex-col items-center justify-center font-bold font-[Chiron_Hei_HK]"
                 @click="openDoc"
               >
-                <img src="../../assets/Home/double_arrow.svg" alt="arrow" />
+                <img src="../../assets/Home/double_arrow.svg" alt="arrow" class="cursor-pointer" />
                 <div class="text-center text-[#B4B4B4] text-xs font-bold font-[Chiron_Hei_HK]">
                   詳細文件參考
                 </div>
@@ -80,7 +91,80 @@
         </div>
       </div>
 
-      <div class="mt-[20vh]"></div>
+      <div class="mt-[8vh]"></div>
+
+      <!-- Card 2 : 創客組 -->
+      <div class="w-[80vw] relative mx-auto select-none mb-[10vh]">
+        <!-- tab ! -->
+        <!-- tab 創客組 -->
+        <div
+          :class="[
+            activeTab2 === '創客組'
+              ? 'bg-[#db8396] left-[61vw] text-[2vw]'
+              : 'bg-[#F4DAE1] left-[61vw] text-[1.6vw]',
+            'w-[14vw] h-[9vh] absolute rounded-tl-[30px] rounded-tr-[30px] cursor-pointer flex items-center justify-center text-white font-black font-[\'Chiron_Hei_HK\']',
+          ]"
+          @click="activeTab2 = '創客組'"
+        >
+          創客交流組
+        </div>
+
+        <!-- tab 工作坊 -->
+        <div
+          :class="[
+            activeTab2 === '工作坊' ? 'bg-[#db8396] text-[2vw]' : 'bg-[#F4DAE1] text-[1.6vw]',
+            'left-[49vw] w-[12vw] h-[9vh] absolute rounded-tl-[30px] rounded-tr-[30px] cursor-pointer flex items-center justify-center text-white font-black font-[\'Chiron_Hei_HK\']',
+          ]"
+          @click="activeTab2 = '工作坊'"
+        >
+          工作坊
+        </div>
+
+        <!-- 卡片背景 -->
+        <!-- left-[7vw] 差不多在中間左右 !-->
+        <!-- 要使用 mt 才可以撐開距離 !! 如果只使用 top 的話只是離父容器 top 多少，但超過的部分部會有撐開的功能 ! -->
+        <!-- 為了讓切換 tag 的時候大小不變 => 設 vh -->
+        <div
+          class="w-[68vw] h-[45vh] mt-[9vh] ml-[7vw] relative bg-neutral-100 rounded-tl-2xl rounded-bl-2xl rounded-br-2xl"
+        >
+          <!-- content -->
+          <div class="pl-[3vw] pr-[3vw] pt-[5vh] pb-[5vh] flex flex-row">
+            <!-- 內容區 -->
+            <template v-if="activeTab2 === '創客組'">
+              <div class="content w-[29vw] h-[35vh] bg-zinc-300 rounded-[30px]"></div>
+
+              <div
+                class="content w-[30vw] text-[#656565] text-[1.65vw] flex items-center justify-center font-bold font-[Chiron_Hei_HK]"
+              >
+                <!-- 字置中 ! -->
+                由新竹市政府命題，融入新竹居民的民生交通情境，鼓勵參賽者跨領域，運用共享、共創的資源，
+                實際動手解決問題。過程中將促進各方人才創作交流，激發新穎想法並提升日常生活的用戶體驗，
+                打造數位科技的創客精神！
+              </div>
+            </template>
+
+            <template v-else>
+              <div
+                class="mt-[3vh] ml-[3vw] mb-[3vh] mr-[3vw] w-[35vw] text-[#656565] text-[1.65vw] flex items-center justify-center font-bold font-[Chiron_Hei_HK]"
+              >
+                由市府團隊主辦，工作坊將包含技術教學、題目講解等內容，並包含參賽團隊選題之流程。
+                過程中參賽者將與講師討論應用技術，共同激盪出更多的靈感與創意。點擊右側箭頭查看詳細內容。
+              </div>
+
+              <!-- info -->
+              <div
+                class="w-[30vw] h-[35vh] flex flex-col items-center justify-center font-bold font-[Chiron_Hei_HK]"
+                @click="openDoc2"
+              >
+                <img src="../../assets/Home/double_arrow.svg" alt="arrow" class="cursor-pointer" />
+                <div class="text-center text-[#B4B4B4] text-xs font-bold font-[Chiron_Hei_HK]">
+                  詳細文件參考
+                </div>
+              </div>
+            </template>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -92,10 +176,14 @@ defineOptions({
   name: 'GroupSection',
 })
 const activeTab = ref('黑客組')
-// const activeTab2 = ref("創客組");
+const activeTab2 = ref('創客組')
 
 function openDoc() {
   console.log('open doc')
+}
+
+function openDoc2() {
+  console.log('open doc2')
 }
 </script>
 
