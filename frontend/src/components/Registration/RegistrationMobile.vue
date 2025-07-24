adc7086a08308f416db6be43c7b6a2b0fa033332
ykchiu2486 <roger2486.cs13@nycu.edu.tw>
2025-07-24 09:30:42 +0800

    feat: update RegistrationMobile with Title and Wings

--- frontend/src/components/Registration/RegistrationMobile.vue
+++ frontend/src/components/Registration/RegistrationMobile.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="registration-mobile">
-    <h2>行動版報名資訊</h2>
-    <p>這裡是行動版的報名頁面內容。</p>
+  <!-- Big Word-->
+  <div
+    class="w-full min-h-[80px] bg-cover bg-center bg-no-repeat flex items-center justify-center"
+    style="background-color: white; position: relative"
+  >
+    <!-- Wings -->
+    <img
+      src="/src/assets/Registration/wingleft.svg"
+      class="absolute pointer-events-none z-20 h-auto scale-120 top-[45%] right-[50%] w-[20vw] wing-animate-left"
+    />
+    <img
+      src="/src/assets/Registration/wingright.svg"
+      class="absolute pointer-events-none z-20 h-auto scale-120 top-[45%] left-[50%] w-[20vw] wing-animate-right"
+    />
+
+    <div class="relative z-10 flex items-center justify-center w-full">
+      <img
+        src="/src/assets/Registration/Title.svg"
+        alt="報名方式"
+        class="w-[40vw] h-auto scale-60"
+      />
+    </div>
   </div>
 </template>
 
@@ -14,6 +33,4 @@ export default RegistrationMobile
 export { RegistrationMobile }
 </script>
 
-<style scoped>
-/* Add your mobile styles here */
-</style>
+<style scoped></style>
