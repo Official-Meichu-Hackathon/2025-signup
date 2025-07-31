489cbaf1c373fb390d59df3be2255a083bdabbc2
Windmill10 <eason200506@gmail.com>
2025-07-31 15:47:15 +0800

    chore: updated logos

--- frontend/src/components/home/BottomSection.vue
+++ frontend/src/components/home/BottomSection.vue
@@ -376,62 +376,47 @@
         </div>
 
         <div>特殊贊助</div>
-        <div class="marquee-sponsors mx-auto overflow-hidden whitespace-nowrap w-[60vw]">
+        <div class="marquee-sponsors mx-auto flex justify-center w-[60vw]">
           <div class="marquee-content-special-sponsor flex-shrink-0">
             <!-- First set of sponsors -->
             <div class="sponsor-item">
-              <span class="sponsor-level">Platinum</span>
+              <span class="sponsor-level" style="margin-left: -0.4rem">Platinum</span>
               <img
                 class="inline-block"
-                src="../../assets/Home/special_sponsors/BABI_platinum.svg"
+                src="../../assets/Home/special_sponsors/BABI_platinum.webp"
                 alt="BABI"
               />
             </div>
             <div class="sponsor-item">
-              <span class="sponsor-level">Silver</span>
+              <span class="sponsor-level" style="margin-left: 1.4rem">Silver</span>
               <img
                 class="inline-block"
-                src="../../assets/Home/special_sponsors/GCOMM_silver.svg"
+                src="../../assets/Home/special_sponsors/GCOMM_silver.webp"
                 alt="GCOMM"
-              />
-            </div>
-            <div class="sponsor-item">
-              <span class="sponsor-level">Others</span>
-              <img
-                class="inline-block"
-                src="../../assets/Home/special_sponsors/hsinhao_others.svg"
-                alt="Hsinhao"
               />
             </div>
           </div>
 
           <!-- Duplicate set for seamless loop -->
-          <div class="marquee-content-special-sponsor flex-shrink-0">
+          <!-- <div class="marquee-content-special-sponsor flex-shrink-0">
             <div class="sponsor-item">
-              <span class="sponsor-level">Platinum</span>
+              <span class="sponsor-level" style="margin-left: -0.2rem;">Platinum</span>
+
               <img
                 class="inline-block"
-                src="../../assets/Home/special_sponsors/BABI_platinum.svg"
+                src="../../assets/Home/special_sponsors/BABI_platinum.webp"
                 alt="BABI"
               />
             </div>
             <div class="sponsor-item">
-              <span class="sponsor-level">Silver</span>
+              <span class="sponsor-level" style="margin-left: 0.4rem;">Silver</span>
               <img
                 class="inline-block"
-                src="../../assets/Home/special_sponsors/GCOMM_silver.svg"
+                src="../../assets/Home/special_sponsors/GCOMM_silver.webp"
                 alt="GCOMM"
               />
             </div>
-            <div class="sponsor-item">
-              <span class="sponsor-level">Others</span>
-              <img
-                class="inline-block"
-                src="../../assets/Home/special_sponsors/hsinhao_others.svg"
-                alt="Hsinhao"
-              />
-            </div>
-          </div>
+          </div> -->
         </div>
 
         <div>媒體合作</div>
@@ -688,7 +673,6 @@ watch(Play, (newVal) => {
 
 /* Add this to your existing CSS */
 .marquee-content-special-sponsor {
-  animation-duration: 9s; /* Half the speed of the normal 12s */
   animation-iteration-count: infinite;
   animation-name: marquee;
   animation-timing-function: linear;
