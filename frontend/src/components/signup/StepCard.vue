b7bfcc471f7872a4bcb2bb18f87bbba4e97785b8
PikachuTW <82265690+PikachuTW@users.noreply.github.com>
2025-08-03 03:54:10 +0800

    fix(signup): text is a bit larger

--- frontend/src/components/signup/StepCard.vue
+++ frontend/src/components/signup/StepCard.vue
@@ -27,7 +27,7 @@ const handleClick = () => {
 
 <template>
   <div
-    class="flex-1 h-26 flex items-center justify-center flex-col text-white rounded-4xl text-md min-w-0 transition-colors duration-200"
+    class="flex-1 h-26 flex items-center justify-center flex-col text-white rounded-4xl text-md xl:text-lg min-w-0 transition-colors duration-200"
     :class="[
       current ? 'bg-darkblue' : 'bg-[#D9D9D9]',
       clickable ? 'cursor-pointer hover:opacity-80' : 'cursor-default',
